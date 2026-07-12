5a27643b9f02052c5694ebae5f4a3a9b1915f424
chillzy998 <mj3918186@gmail.com>
2026-07-13 00:16:53 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
 const firebaseConfig = {
+
+    const firebaseConfig = {
   apiKey: "AIzaSyC3JfmwweJj3FcaPgzfpwxiMJwZm_hX7r8",
   authDomain: "wis-website-82f3d.firebaseapp.com",
   projectId: "wis-website-82f3d",
@@ -16,5 +19,14 @@ const firebaseConfig = {
   measurementId: "G-KWGLD5Z410"
 };
 
-// Initialize Firebase
+};
+
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+const storage = getStorage(app);
+
+export { db, auth, storage };
