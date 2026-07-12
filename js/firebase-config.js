7ac01973d56084d525f6a6cf06a09fdaa6a30edf
chillzy998 <mj3918186@gmail.com>
2026-07-13 07:25:42 +0800

    Fix formatting and structure in firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,17 @@
-// Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyC3JfmwweJj3FcaPgzfpwxiMJwZm_hX7r8",
+    apiKey: "AIzaSyC3JfmwweJj3FcaPgzfpwxiMJwZm_hX7r8",
   authDomain: "wis-website-82f3d.firebaseapp.com",
   projectId: "wis-website-82f3d",
   storageBucket: "wis-website-82f3d.firebasestorage.app",
   messagingSenderId: "1085696949595",
   appId: "1:1085696949595:web:69c0e2f2e4ee28220dae7e",
   measurementId: "G-KWGLD5Z410"
-};
 
 };
 
